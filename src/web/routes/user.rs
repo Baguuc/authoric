c84957e6722998a696af5ed716dbde0f862268ea
Jan Żukowski <janzukowski@proton.me>
2025-01-06 10:26:56 +0100

    update: added DELETE /users/{login} route

--- src/web/routes/user.rs
+++ src/web/routes/user.rs
@@ -1,4 +1,4 @@
-use actix_web::{delete, http::StatusCode, post, web::{Data, Json, Query}, Responder};
+use actix_web::{delete, http::StatusCode, post, web::{Data, Json, Path, Query}, Responder};
 use serde::Deserialize;
 use serde_json::{json, Value};
 
@@ -39,6 +39,57 @@ pub async fn post_users(
     Err(_) => return ServerResponse::new(
       StatusCode::BAD_REQUEST,
       None
+    )
+  };
+
+  return ServerResponse::new(
+    StatusCode::OK,
+    None
+  );
+}
+
+#[derive(Deserialize)]
+pub struct DeleteGroupQueryData {
+    session_token: String
+}
+
+#[delete("/users/{login}")]
+pub async fn delete_users(
+    query: Query<DeleteGroupQueryData>,
+    name: Path<String>,
+    data: Data<CauthConfig>
+) -> impl Responder {
+    // these will never error
+  let mut db_conn = data.db_conn
+  .acquire()
+  .await
+  .unwrap();
+
+  let permitted = LoginSession::has_permission(
+    &mut db_conn,
+    &query.session_token,
+    &format!("users:delete:{}", name)
+  )
+  .await;
+  
+  if !permitted {
+    return ServerResponse::new(
+      StatusCode::UNAUTHORIZED,
+      None
+    );
+  }
+
+  let result = User::delete(
+    &mut db_conn,
+    (&name).to_string()
+  )
+  .await;
+
+  match result {
+    Ok(_) => (),
+    Err(_) => return ServerResponse::new(
+        StatusCode::BAD_REQUEST,
+        None
     )
   };
 
