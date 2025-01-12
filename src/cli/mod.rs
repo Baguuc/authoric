6e60fa24e55dbb0f46a516c135d3f20821f97f2f
Jan Żukowski <janzukowski@proton.me>
2025-01-12 15:37:41 +0100

    update: added defaults initialization

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,7 +6,7 @@ use clap::{
 };
 use crate::{cli::subcommands::{
   admin::AdminCommand, config::ConfigCommand, daemon::DaemonCommand, run::RunCommand
-}, config::CauthConfig};
+}, config::CauthConfig, models::{Group, Permission}};
 
 
 
@@ -48,4 +48,87 @@ pub enum ActionType {
   Daemon(DaemonCommand),
   Config(ConfigCommand),
   Admin(AdminCommand)
+}
+
+
+pub async fn init_defaults(config: &CauthConfig) {
+  let mut tx = config.db_conn
+    .begin()
+    .await
+    .unwrap();
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:permissions:get".to_string(), 
+    &"permission to retrieve the permission list from the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:permissions:post".to_string(), 
+    &"permission to post new permission to the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:permissions:delete".to_string(), 
+    &"permission to delete a permission from the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:groups:get".to_string(), 
+    &"permission to retrieve the groups list from the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:groups:post".to_string(), 
+    &"permission to post new group to the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:groups:delete".to_string(), 
+    &"permission to post new group to the database".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:groups:update".to_string(), 
+    &"permission to grant/revoke permissions to groups".to_string()
+  )
+  .await;
+
+  let _ = Permission::insert(
+    &mut tx,
+    &"cauth:users:update".to_string(), 
+    &"permission to grant/revoke groups to users".to_string()
+  )
+  .await;
+
+  let _ = Group::insert(
+    &mut tx,
+    &"root".to_string(), 
+    &"the most privileged group, having to permissions to do everything. Caution: do not grant this group to any untrusted user as it can result in damages done to your system. Instead, create their own group fitting their needs.".to_string(),
+    &vec![
+      "cauth:permissions:get".to_string(),
+      "cauth:permissions:post".to_string(),
+      "cauth:permissions:delete".to_string(),
+      "cauth:groups:get".to_string(),
+      "cauth:groups:post".to_string(),
+      "cauth:groups:delete".to_string(),
+      "cauth:groups:update".to_string(),
+      "cauth:users:update".to_string()
+    ]
+  )
+  .await;
+
+  let _ = tx.commit().await;
 }
--- src/cli/subcommands/run/mod.rs
+++ src/cli/subcommands/run/mod.rs
@@ -1,7 +1,7 @@
 use clap::Args;
 use futures::executor::block_on;
 
-use crate::{config::CauthConfig, web::run_server};
+use crate::{cli::init_defaults, config::CauthConfig, web::run_server};
 
 
 #[derive(Debug, Args)]
@@ -12,6 +12,7 @@ pub struct RunCommand {
 
 impl RunCommand {
   pub fn run(self, config: CauthConfig) {
+    block_on(init_defaults(&config));
     let _ = block_on(run_server(config));
   }
 }
